770e0b0eb2f14d3b884989d0ffe899e66d3189d1
Samer Zukari <samer.zukari@outlook.de>
2024-10-06 17:29:02 +0200

    Initial commit

---
+++ src/pages/logIn/LogIn.jsx
@@ -0,0 +1,24 @@
+import "./logIn.css";
+
+export default function LogIn() {
+  return (
+    <div id="app">
+      <h1>User Login</h1>
+      <p>
+        <label>Email</label>
+        {/* You don't need to do anything with those inputs! You'll learn how to handle user input later */}
+        <input type="email" />
+      </p>
+
+      <p>
+        <label>Password</label>
+        {/* You don't need to do anything with those inputs! You'll learn how to handle user input later */}
+        <input type="password" />
+      </p>
+
+      <p id="actions">
+        <button>Login</button>
+      </p>
+    </div>
+  );
+}
